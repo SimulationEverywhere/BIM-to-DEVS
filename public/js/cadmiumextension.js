21d071d8337cabc0626ce1254eca59b559188b99
khvilaboa <khvilaboa@gmail.com>
2020-07-13 19:48:28 +0200

    Counting walls...

--- public/js/cadmiumextension.js
+++ public/js/cadmiumextension.js
@@ -34,22 +34,29 @@ class CadmiumExtension extends Autodesk.Viewing.Extension {
         this._button = new Autodesk.Viewing.UI.Button('co2CadmiumExtensionButton');
         this._button.onClick = (ev) => {
             //alert("Not implemented yet! It has to export the model information to a Cadmium JSON file (CO2 model).");
+            // For now I count the walls here...
+
             // Get current selection
             const selection = this.viewer.getSelection();
             this.viewer.clearSelection();
+            var countWalls = 0;
+
             // Anything selected?
             if (selection.length > 0) {
-                // Iterate through the list of selected dbIds
-                selection.forEach((dbId) => {
-                    // Get properties of each dbId
-                    this.viewer.getProperties(dbId, (props) => {
-                        // Output properties to console
-                        console.log(props);
-                    });
+                viewer.model.getBulkProperties(selection, ['Category'],
+                function(elements){
+                    var totalWalls = 0;
+                    for(var i=0; i<elements.length; i++){
+                    console.log(elements[i].properties[0].displayValue);
+                        if(elements[i].properties[0].displayValue == "Revit Walls") {
+                                totalWalls += 1;//elements[i].properties[0].displayValue;
+                        }
+                    }
+                    console.log("Total walls: " + totalWalls);
+                    alert("Total walls: " + totalWalls);
                 });
             } else {
-                // If nothing selected, restore
-                this.viewer.isolate(0);
+                alert("Nothing selected.");
             }
         };
         this._button.setToolTip('Export JSON for CO2 model');
@@ -60,7 +67,6 @@ class CadmiumExtension extends Autodesk.Viewing.Extension {
         this._button = new Autodesk.Viewing.UI.Button('epidemicsCadmiumExtensionButton');
         this._button.onClick = (ev) => {
             // Execute an action here
-
             alert("Not implemented yet! It has to export the model information to a Cadmium JSON file (Epidemic model).");
         };
         this._button.setToolTip('Export JSON for Epidemic model');

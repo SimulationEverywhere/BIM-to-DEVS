bc56fea69b15bff49bfee96197f4fb30404abcfa
khvilaboa <khvilaboa@gmail.com>
2020-07-13 16:35:03 +0200

    Initial commit
    Web viewer and extension buttons

---
+++ public/js/cadmiumextension.js
@@ -0,0 +1,57 @@
+class CadmiumExtension extends Autodesk.Viewing.Extension {
+    constructor(viewer, options) {
+        super(viewer, options);
+        this._group = null;
+        this._button = null;
+    }
+
+    load() {
+        console.log('CadmiumExtension has been loaded');
+        return true;
+    }
+
+    unload() {
+        // Clean our UI elements if we added any
+        if (this._group) {
+            this._group.removeControl(this._button);
+            if (this._group.getNumberOfControls() === 0) {
+                this.viewer.toolbar.removeControl(this._group);
+            }
+        }
+        console.log('CadmiumExtension has been unloaded');
+        return true;
+    }
+
+    onToolbarCreated() {
+        // Create a new toolbar group if it doesn't exist
+        this._group = this.viewer.toolbar.getControl('allCadmiumExtensionToolbar');
+        if (!this._group) {
+            this._group = new Autodesk.Viewing.UI.ControlGroup('allCadmiumExtensionToolbar');
+            this.viewer.toolbar.addControl(this._group);
+        }
+
+        // Button for exporting CO2 JSON configuration files
+        this._button = new Autodesk.Viewing.UI.Button('co2CadmiumExtensionButton');
+        this._button.onClick = (ev) => {
+            // Execute an action here
+
+            alert("Not implemented yet! It has to export the model information to a Cadmium JSON file (CO2 model).");
+        };
+        this._button.setToolTip('Export JSON for CO2 model');
+        this._button.addClass('co2CadmiumExtensionIcon');
+        this._group.addControl(this._button);
+
+        // Button for exporting Epidemic JSON configuration files
+        this._button = new Autodesk.Viewing.UI.Button('epidemicsCadmiumExtensionButton');
+        this._button.onClick = (ev) => {
+            // Execute an action here
+
+            alert("Not implemented yet! It has to export the model information to a Cadmium JSON file (Epidemic model).");
+        };
+        this._button.setToolTip('Export JSON for Epidemic model');
+        this._button.addClass('epidemicsExtensionIcon');
+        this._group.addControl(this._button);
+    }
+}
+
+Autodesk.Viewing.theExtensionManager.registerExtension('CadmiumExtension', CadmiumExtension);

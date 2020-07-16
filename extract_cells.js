f97b72aa2a69ba8228a534c4876f3fc38403c942
xlirate <Griffin-Barrett@outlook.com>
2020-07-16 15:37:56 -0400

    Adding dependancies for the server side geometry extration

---
+++ extract_cells.js
@@ -0,0 +1,25 @@
+const yargs = require('yargs');
+
+/*
+const argv = yargs
+    .command('lyr', 'Tells whether an year is leap year or not', {
+        year: {
+            description: 'the year to check for',
+            alias: 'y',
+            type: 'number',
+        }
+    })
+    .option('time', {
+        alias: 't',
+        description: 'Tell the present Time',
+        type: 'boolean',
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+
+console.log(process.argv)
+console.log(process.env.FORGE_CLIENT_ID)
+console.log(process.env.FORGE_CLIENT_SECRET)
+*/

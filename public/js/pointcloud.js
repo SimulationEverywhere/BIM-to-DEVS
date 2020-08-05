bffa97cf30853caa094737a3cccfa03b71700e96
Mitali92 <60850783+Mitali92@users.noreply.github.com>
2020-08-05 16:19:23 -0400

    Removed the Point Cloud from onload of the model
    Point Cloud will be loaded only after model loads.

--- public/js/pointcloud.js
+++ public/js/pointcloud.js
@@ -11,12 +11,18 @@ class PointCloudExtension extends Autodesk.Viewing.Extension {
     }
 
     load() {
-        this.points = this._generatePointCloud();
-        this.points.scale.set(108,73, 5); //Match size with project size. (unit: inch)
-        this.viewer.impl.createOverlayScene('pointclouds');
-        this.viewer.impl.addOverlay('pointclouds', this.points);
-
+        window.renderPointCloud = false;
+        this._renderCloud();
         return true;
+    }
+
+    _renderCloud(){
+        if(renderPointCloud){
+            this.points = this._generatePointCloud();
+            this.points.scale.set(108,73, 5); //Match size with project size. (unit: inch)
+            this.viewer.impl.createOverlayScene('pointclouds');
+            this.viewer.impl.addOverlay('pointclouds', this.points);
+        }
     }
 
     unload() {
@@ -31,6 +37,7 @@ class PointCloudExtension extends Autodesk.Viewing.Extension {
         return true;
     }
 
+    
     /**
      * Generates {@link https://github.com/mrdoob/three.js/blob/r71/src/core/BufferGeometry.js|BufferGeometry}
      * with (_width_ x _length_) positions and varying colors. The resulting geometry will span from -0.5 to 0.5
@@ -48,22 +55,6 @@ class PointCloudExtension extends Autodesk.Viewing.Extension {
 		let positions = new Float32Array(numPoints * 3);
         let colors = new Float32Array(numPoints * 3);
         let color = new THREE.Color();
-        // let k = 0;
-        
-        // for (var i = 0; i < max_x; i++) {
-        //     for (var j = 0; j < max_y; j++) {
-        //         let u = i / max_x - 0.52;
-        //         let v = j / max_y - 0.52;
-
-        //         positions[3 * k] = u;
-        //         positions[3 * k + 1] = v;
-        //         positions[3 * k + 2] = this.zaxisOffset;
-				
-		// 		color.setRGB(1, 1, 1);
-        //         color.toArray(colors, k * 3);
-        //         k++;
-        //     }
-        // }
 
         for (var i = 0; i < data.length; i++){
             var messages = data[i];
@@ -139,10 +130,14 @@ class PointCloudExtension extends Autodesk.Viewing.Extension {
         // Add a new button to the toolbar group
         this._button = new Autodesk.Viewing.UI.Button('myAwesomeExtensionButton');
         this._button.onClick = (ev) => {
+            window.renderPointCloud = true;
+
+            this._renderCloud();
             if(window.legendOFF){
                 this._appearLegend();
                 window.legendOFF = false;
             }
+
             var i = 0;
                 
             var interval = setInterval(() => {
@@ -214,69 +209,69 @@ class PointCloudExtension extends Autodesk.Viewing.Extension {
     }
 
     _appearLegend(){
-        var colorScale = d3.scaleLinear()
-        .domain([0,	10,	15,	20, 25, 100])
-        .range(['#E28672', '#EC93AB', '#CEB1DE', '#95D3F0', '#77EDD9', '#A9FCAA']);
-  
-    // append a defs (for definition) element to your SVG
-      var svgLegend = d3.select('#legend').append('svg');
-      var defs = svgLegend.append('defs');
-  
-      // append a linearGradient element to the defs and give it a unique id
-      var linearGradient = defs.append('linearGradient')
-          .attr('id', 'linear-gradient');
-  
-      // horizontal gradient
-      linearGradient
-          .attr("x1", "0%")
-          .attr("y1", "0%")
-          .attr("x2", "100%")
-          .attr("y2", "0%");
-  
-      // append multiple color stops by using D3's data/enter step
-      linearGradient.selectAll("stop")
-          .data([
-              {offset: "0%", color: "#0000FF"},
-              {offset: "36.36%", color: "#FFFFBE"},
-              {offset: "100%", color: "#FF0000"}
-          ])
-          .enter().append("stop")
-          .attr("offset", function(d) { 
-              return d.offset; 
-          })
-          .attr("stop-color", function(d) { 
-              return d.color; 
-          });
-  
-      // append title
-      svgLegend.append("text")
-          .attr("class", "legendTitle")
-          .attr("x", 0)
-          .attr("y", 20)
-          .style("text-anchor", "mid")
-          .text("CO2 Concentration (ppm)");
-  
-      // draw the rectangle and fill with gradient
-      svgLegend.append("rect")
-          .attr("x", 0)
-          .attr("y", 30)
-          .attr("width", 300)
-          .attr("height", 15)
-          .style("fill", "url(#linear-gradient)");
-  
-        //create tick marks
-        var xLeg = d3.scale.ordinal()
-        .domain([100,500,1200])
-        .range([0,100,290])
+            var colorScale = d3.scaleLinear()
+                 .domain([0,	10,	15,	20, 25, 100])
+                 .range(['#E28672', '#EC93AB', '#CEB1DE', '#95D3F0', '#77EDD9', '#A9FCAA']);
+        
+            // append a defs (for definition) element to your SVG
+            var svgLegend = d3.select('#legend').append('svg');
+            var defs = svgLegend.append('defs');
+        
+            // append a linearGradient element to the defs and give it a unique id
+            var linearGradient = defs.append('linearGradient')
+                .attr('id', 'linear-gradient');
+        
+            // horizontal gradient
+            linearGradient
+                .attr("x1", "0%")
+                .attr("y1", "0%")
+                .attr("x2", "100%")
+                .attr("y2", "0%");
+        
+            // append multiple color stops by using D3's data/enter step
+            linearGradient.selectAll("stop")
+                .data([
+                    {offset: "0%", color: "#0000FF"},
+                    {offset: "36.36%", color: "#FFFFBE"},
+                    {offset: "100%", color: "#FF0000"}
+                ])
+                .enter().append("stop")
+                .attr("offset", function(d) { 
+                    return d.offset; 
+                })
+                .attr("stop-color", function(d) { 
+                    return d.color; 
+                });
+        
+            // append title
+            svgLegend.append("text")
+                .attr("class", "legendTitle")
+                .attr("x", 0)
+                .attr("y", 20)
+                .style("text-anchor", "mid")
+                .text("CO2 Concentration (ppm)");
+        
+            // draw the rectangle and fill with gradient
+            svgLegend.append("rect")
+                .attr("x", 0)
+                .attr("y", 30)
+                .attr("width", 300)
+                .attr("height", 15)
+                .style("fill", "url(#linear-gradient)");
+        
+                //create tick marks
+                var xLeg = d3.scale.ordinal()
+                .domain([100,500,1200])
+                .range([0,100,290])
 
-        var axisLeg = d3.axisBottom(xLeg);
+                var axisLeg = d3.axisBottom(xLeg);
 
-        svgLegend
-            .attr("class", "axis")
-            .append("g")
-            .attr("transform", "translate(10, 40)")
-            .call(axisLeg);
-    }
+                svgLegend
+                    .attr("class", "axis")
+                    .append("g")
+                    .attr("transform", "translate(10, 40)")
+                    .call(axisLeg);
+            }
 }
 
 

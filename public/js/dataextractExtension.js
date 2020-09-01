3c053b6eddd20ef2934d696bc8c86a5a600542fb
Mitali92 <60850783+Mitali92@users.noreply.github.com>
2020-09-01 15:31:27 -0400

    Updated file

--- public/js/dataextractExtension.js
+++ public/js/dataextractExtension.js
@@ -36,16 +36,12 @@ class DataExtractExtension extends Autodesk.Viewing.Extension {
         this._button = new Autodesk.Viewing.UI.Button('co2DataExtractExtensionButton');
         this._button.onClick = (ev) => {
 
-        // //single object detection
-        // const selection = this.viewer.getSelection();
-        // this.extractObjDict(selection);
+        //getting the id's of the selected material
+        //const selection = this.viewer.getSelection();
 
         //getting the id's of the material
         var ids = viewer.model.getData().instanceTree.nodeAccess.dbIdToIndex;
         this.extractObjDict(ids);
-
-        //canvas without elementID
-        //this.drawcanvas();
 
         };
         this._button.setToolTip('Export JSON for CO2 model');
@@ -60,72 +56,141 @@ class DataExtractExtension extends Autodesk.Viewing.Extension {
         //elementID matching bounding box
         viewer.model.getBulkProperties(ids, ['Category'],
             function(elements){
-                var dataID = [];
-                var datas = [];
-                for(var d=0; d<elements.length; d++){
-                    var category = elements[d].properties[0].displayValue;
-                    if(category == "Revit Walls" || category == "Revit Windows" || category == "Revit Doors" || category == "Revit Furniture Systems" || category == "Revit Mechanical Equipment") {
-                             dataID.push(elements[d].dbId);
-                             
-                            //  var bbox = thisRef.getBoundingBox(elements[d].dbId);
-                            //  var objType = category.substring(6, category.length-1);
-                            //  datas.push({"id": elements[d].dbId, "type": objType, "bbox": bbox});
+                var data = [];
+                for(var i=0; i<elements.length; i++){
+                    var category = elements[i].properties[0].displayValue;
+                     if(category == "Revit Walls" || category == "Revit Windows" || category == "Revit Doors" || category == "Revit Furniture Systems" || category == "Revit Mechanical Equipment") {
+                        var bbox = thisRef.getBoundingBox(elements[i].dbId);
+                        var objType = category.substring(6, category.length-1);
+                        data.push({"id": elements[i].dbId, "type": objType, "bbox": bbox});
                     }
                 }
-                //DataExtractExtension.formatObjDict(data);  // remove negative values
-                //var dataStr = JSON.stringify(data).replace(/\"([^(\")"]+)\":/g,"$1:");
-                DataExtractExtension.drawcanvas(dataID);
-                //DataExtractExtension.download(dataStr, "config.json", "application/json");
+                DataExtractExtension.formatObjDict(data);  // remove negative values
+                var dataStr = JSON.stringify(data).replace(/\"([^(\")"]+)\":/g,"$1:");
+                DataExtractExtension.drawCanvas(data);
+                DataExtractExtension.download(dataStr, "config.json", "application/json");
             }
         );
     }   
 
-    // getBoundingBox(id) {
-    //     var f = new Float32Array(6);
-    //     var us = this.viewer.model.getUnitScale();
-    //     this.viewer.model.getInstanceTree().getNodeBox(id, f);
-    //     var bbox = f.map((e) => {return e*us});
-    //     bbox = Array.prototype.slice.call(bbox);
-    //     return bbox;
-    // }
-
-    static drawcanvas(dataID) {
-        //ray-shooter bounding box method
-         var cellSize  = 0.25;
-         const bounds = viewer.model.getBoundingBox();
-         const width = Math.floor((bounds.max.x - bounds.min.x) / cellSize);
-         const height = Math.floor((bounds.max.y - bounds.min.y) / cellSize);  
-         const canvas = document.createElement('canvas');
-         canvas.setAttribute('width', width + 'px');
-         canvas.setAttribute('height', height + 'px');
-         const context = canvas.getContext('2d');
-         const data = context.getImageData(0, 0, width, height);
-         let ray = new THREE.Ray(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, -1));
-         let i = 0;
-         for (let j = 0; j < height; j++) {
-             for (let k = 0; k < width; k++) {
-                 ray.origin.x = bounds.min.x + k * cellSize;
-                 ray.origin.y = bounds.min.y + j * cellSize;
-                 const intersection = viewer.impl.rayIntersect(ray, false,dataID);
-                 if (intersection) {
-                     data.data[i] = 0;
-                     data.data[i + 1] = 255;
-                     data.data[i + 2] = 0;
-                     data.data[i + 3] = 255;
-                  } //else {
-                //     data.data[i] = 0;
-                //     data.data[i + 1] = 0;
-                //     data.data[i + 2] = 0;
-                //     data.data[i + 3] = 255;
-                // }
-                 i += 4;
-             }
-         }
-         context.putImageData(data, 0, 0);
-         canvas.style.position = 'absolute';
-         canvas.style.zIndex = 100;
-         document.body.appendChild(canvas);
+    getBoundingBox(id) {
+        var f = new Float32Array(6);
+        var us = this.viewer.model.getUnitScale();
+        this.viewer.model.getInstanceTree().getNodeBox(id, f);
+        var bbox = f.map((e) => {return e*us});
+        bbox = Array.prototype.slice.call(bbox);
+        return bbox;
     }
+
+    // Convert the bboxes coordinates to non-negative ones if needed
+    static formatObjDict(elems) {
+        var dims = DataExtractExtension.getDimFromObjDict(elems);
+
+        for(var i=0; i<elems.length; i++) {
+            for(var j=0; j<3; j++) {
+                if(dims[j] < 0) {
+                    elems[i]["bbox"][j] = elems[i]["bbox"][j] - dims[j];
+                    elems[i]["bbox"][j+3] = elems[i]["bbox"][j+3] - dims[j];
+                }
+            }
+        }
+        return elems;
+    }
+
+    static getDimFromObjDict(elems) {
+        if(elems.length == 0) return;
+        var minX = elems[0]["bbox"][0],
+            minY = elems[0]["bbox"][1],
+            minZ = elems[0]["bbox"][2],
+            maxX = elems[0]["bbox"][3],
+            maxY = elems[0]["bbox"][4],
+            maxZ = elems[0]["bbox"][5];
+
+        for(var i=1; i<elems.length; i++){
+            if(elems[i]["bbox"][0]<minX) minX = elems[i]["bbox"][0];
+            if(elems[i]["bbox"][1]<minY) minY = elems[i]["bbox"][1];
+            if(elems[i]["bbox"][2]<minZ) minZ = elems[i]["bbox"][2];
+            if(elems[i]["bbox"][3]>maxX) maxX = elems[i]["bbox"][3];
+            if(elems[i]["bbox"][4]>maxY) maxY = elems[i]["bbox"][4];
+            if(elems[i]["bbox"][5]>maxZ) maxZ = elems[i]["bbox"][5];
+        }
+
+        return [minX, minY, minZ, maxX, maxY, maxZ];
+    }
+
+    static drawCanvas(elems) {
+        var dims = DataExtractExtension.getDimFromObjDict(elems);
+        var ratio = 30;
+        var canv = document.createElement('canvas');
+        //var prec = 200; // mm (width of each cell in the scenario)
+        canv.id = 'canvasAux';
+        canv.width = dims[3]*ratio;
+        canv.height = dims[4]*ratio;
+        document.body.appendChild(canv); // adds the canvas to the body element
+        var context = canv.getContext('2d');
+
+        for(var i=0; i<elems.length; i++) {  // Draw walls first...
+            if(elems[i] == "Wall")
+            DataExtractExtension.drawItem(context, elems[i], ratio);
+        }
+
+        for(var i=0; i<elems.length; i++) {  // ... and then the rest of the objects
+            if(elems[i] != "Wall")
+            DataExtractExtension.drawItem(context, elems[i], ratio);
+        }
+
+        //canv.style.width = (dims[3]*1000/prec) + 'px';
+        //canv.style.height = (dims[4]*1000/prec) + 'px';
+    }
+
+    static drawItem(context, elem, ratio) {
+        var srcX = elem["bbox"][0]*ratio;
+        var srcY = elem["bbox"][1]*ratio;
+        var dstX = elem["bbox"][3]*ratio;
+        var dstY = elem["bbox"][4]*ratio;
+
+        if(elem["type"] == "Wall") {
+            context.strokeStyle = "#000000";
+        } else if(elem["type"] == "Window") {
+            context.strokeStyle = "#FF0000";
+        } else if(elem["type"] == "Door") {
+            context.strokeStyle = "#00FF00";
+        }
+
+        if(dstX-srcX < dstY-srcY) {
+            context.lineWidth = dstX-srcX;
+            srcX += context.lineWidth/2;
+            dstX -= context.lineWidth/2;
+        } else {
+            context.lineWidth = dstY-srcY;
+            srcY += context.lineWidth/2;
+            dstY -= context.lineWidth/2;
+        }
+        context.beginPath();
+        context.moveTo(srcX, srcY);
+        context.lineTo(dstX, dstY);
+        context.stroke();
+        context.closePath();
+    }
+
+    static download(data, filename, type) {
+        var file = new Blob([data], {type: type});
+        if (window.navigator.msSaveOrOpenBlob) // IE10+
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        else { // Others
+            var a = document.createElement("a"),
+                    url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 0);
+        }
+    }
+
 
 }
 

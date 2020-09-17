39a11ef6a86cbe1085f22ab703d4275cd314ca07
Mitali92 <60850783+Mitali92@users.noreply.github.com>
2020-09-17 13:13:55 -0400

    Add files via upload

--- public/js/dataextractExtension.js
+++ public/js/dataextractExtension.js
@@ -37,15 +37,13 @@ class DataExtractExtension extends Autodesk.Viewing.Extension {
         this._button.onClick = (ev) => {
 
         // //single object detection
-        const selection = this.viewer.getSelection();
+        //const selection = this.viewer.getSelection();
         // this.extractObjDict(selection);
 
         //getting the id's of the material
         var ids = viewer.model.getData().instanceTree.nodeAccess.dbIdToIndex;
         this.extractObjDict(ids);
 
-        //canvas without elementID
-        //this.drawcanvas();
 
         };
         this._button.setToolTip('Export JSON for CO2 model');
@@ -64,23 +62,39 @@ class DataExtractExtension extends Autodesk.Viewing.Extension {
                 var datas = [];
                 for(var d=0; d<elements.length; d++){
                     var category = elements[d].properties[0].displayValue;
-                    if(category == "Revit Walls" || category == "Revit Windows" || category == "Revit Doors" || category == "Revit Furniture Systems" || category == "Revit Mechanical Equipment") { 
-                             dataID.push(elements[d].dbId);
+                    if(category == "Revit Walls" || category == "Revit Windows" || category == "Revit Doors" || category == "Revit Mechanical Equipment") { 
+                            dataID.push(elements[d].dbId);
                              var objType = category.substring(6, category.length-1);
                              datas.push({"id": elements[d].dbId, "type": objType});
                     }
                 }
-                //DataExtractExtension.formatObjDict(data);  // remove negative values
-                //var dataStr = JSON.stringify(data).replace(/\"([^(\")"]+)\":/g,"$1:");
-                DataExtractExtension.drawcanvas(dataID,datas);
-                //DataExtractExtension.download(dataStr, "config.json", "application/json");
+                DataExtractExtension.getDBID_ChairsOnly(dataID, datas);
             }
         );
     }   
+    
+    static getDBID_ChairsOnly(dataID, datas) {
+        function doneSearch(dbids) {
+            // dbids here are all the chairs
+            // this is where you do a rayhit
+            var dbIDS = [];
+            var arr =[];
+            dbIDS = dataID.concat(dbids);
+            
+            for(let i = 0; i < dbids.length; i++){
+                arr.push({"id": dbids[i], "type": 'Chair'});
+            }
 
-    static drawcanvas(dataID,datas) {
+            Array.prototype.push.apply(datas,arr);
+            DataExtractExtension.drawcanvas(dbIDS, datas);
+        }
+        viewer.search('"Chair model 01 ["', dbids => {
+            doneSearch(dbids)}, null, ["name"] );
+    }
+
+    static drawcanvas(dataID, datas) {
         //ray-shooter bounding box method
-         var cellSize  = 0.2;
+         var cellSize  = 0.25;
          const bounds = viewer.model.getBoundingBox();
          const width = Math.floor((bounds.max.x - bounds.min.x) / cellSize);
          const height = Math.floor((bounds.max.y - bounds.min.y) / cellSize);  
@@ -99,31 +113,86 @@ class DataExtractExtension extends Autodesk.Viewing.Extension {
                  const intersection = viewer.impl.rayIntersect(ray, false, dataID);
                 
                   if (intersection){
-                      for(let g = 0; g < datas.length; g++){
-                        if(intersection.dbId == datas[g].id){
-                            intersectionData.push({'dbID': intersection.dbId, "type":datas[g].type, "vector":intersection.intersectPoint});
-                        }
-                      }
-                     data.data[i] = 0;
-                     data.data[i + 1] = 0;
-                     data.data[i + 2] = 255;
-                     data.data[i + 3] = 255;
+
+                    //getting the intersected points to be used for Cadmium input 
+                       for(let c = 0; c < datas.length; c++) {
+                            if(datas[c].id == intersection.dbId)
+                               intersectionData.push({"point":intersection.intersectPoint, "dbID":intersection.dbId, "type":datas[c].type});
+                         }
+
+                            // COLOR THE OBJECTS IN THE IMAGE
+                            var objType=datas.filter((row)=>{
+                                if(row.id==intersection.dbId){
+                                    return row;
+                                }
+                            });
+                            var type="";
+                            if(objType.length>0){
+                                type=objType[0].type;
+                            }
+                            if(type=="Wall"){
+                                data.data[i] = 0;
+                                data.data[i + 1] = 0;
+                                data.data[i + 2] = 0;
+                                data.data[i + 3] = 255;
+                            }else if(type=="Window"){
+                                data.data[i] = 255;
+                                data.data[i + 1] = 0;
+                                data.data[i + 2] = 0;
+                                data.data[i + 3] = 255;
+                            }else if(type=="Door"){
+                                data.data[i] = 0;
+                                data.data[i + 1] = 255;
+                                data.data[i + 2] = 0;
+                                data.data[i + 3] = 255;
+                            }
+                            else if(type=="Chair"){
+                                data.data[i] = 255;
+                                data.data[i + 1] = 0;
+                                data.data[i + 2] = 255;
+                                data.data[i + 3] = 255;
+                            }
+                            else if(type=="Mechanical Equipmen"){
+                                data.data[i] = 0;
+                                data.data[i + 1] = 0;
+                                data.data[i + 2] = 255;
+                                data.data[i + 3] = 255;
+                            }
+                            else{
+                            data.data[i] = 0;
+                            data.data[i + 1] = 255;
+                            data.data[i + 2] = 255;
+                            data.data[i + 3] = 255;
+                            }
                   }
-                
-                //    else {
-                //     data.data[i] = 0;
-                //     data.data[i + 1] = 0;
-                //     data.data[i + 2] = 0;
-                //     data.data[i + 3] = 255;
-                // }
                  i += 4;
              }
          }
-         console.log(intersectionData);
+         //console.log(intersectionData);
+         var dataStr = JSON.stringify(intersectionData).replace(/\"([^(\")"]+)\":/g,"$1:");
+         DataExtractExtension.download(dataStr, "config.json", "application/json");
          context.putImageData(data, 0, 0);
          canvas.style.position = 'absolute';
          canvas.style.zIndex = 100;
          document.body.appendChild(canvas);
+    }
+
+    static download(data, filename, type) {
+        var file = new Blob([data], {type: type});
+        if (window.navigator.msSaveOrOpenBlob) // IE10+
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        else { // Others
+            var a = document.createElement("a"),
+                    url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 0);
+        }
     }
 
 }
